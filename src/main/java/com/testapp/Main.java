7d84701448ab39b9a963036e5595caea748a94ef
Spectrum Project <noob.online@yandex.ru>
2021-03-09 01:38:16 +0400

    Improved parseText method builds the TreeMap containing words usage statistics without creating List, thus increasing the program speed & memory usage

--- src/main/java/com/testapp/Main.java
+++ src/main/java/com/testapp/Main.java
@@ -1,7 +1,7 @@
 package com.testapp;
 
-import java.util.List;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +10,12 @@ public class Main {
         String url = scanner.nextLine();
         String textFromUrl = TextInterpreter.getTextFromUrl(url);
         if (!textFromUrl.equals("")) {
-            List<String> words = TextInterpreter.parseText(textFromUrl);
             System.out.println("Статистика использования уникальных слов: ");
-            TextInterpreter.printTreeMap(TextInterpreter.getWordStatsFromList(words));
+            printTreeMap(TextInterpreter.parseText(textFromUrl));
         }
+    }
+
+    public static void printTreeMap (TreeMap<String, Integer> treeMap) {
+        treeMap.forEach((k, v) -> System.out.println(k + " - " + v));
     }
 }
--- src/main/java/com/testapp/TextInterpreter.java
+++ src/main/java/com/testapp/TextInterpreter.java
@@ -12,51 +12,36 @@ public class TextInterpreter {
     public static String getTextFromUrl (String url) {
         try {
             Document doc = Jsoup.connect(url)
+                    .maxBodySize(0)
                     .userAgent("Chrome/4.0.249.0 Safari/532.5")
                     .referrer("http://www.google.com")
                     .get();
+            Jsoup.parse(doc.outerHtml()).wholeText();
             return Jsoup.parse(doc.outerHtml()).wholeText();
         }
         catch (IllegalArgumentException iae) {
-            System.out.println("Неверный адрес веб-страницы");
+            System.err.println("Неверный адрес веб-страницы");
             iae.printStackTrace();
         }
         catch (UnknownHostException uhe) {
-            System.out.println("Неверный хост");
+            System.err.println("Неверный хост");
             uhe.printStackTrace();
         }
         catch (IOException ioe) {
-            System.out.println("Ошибка ввода/вывода");
+            System.err.println("Ошибка ввода/вывода");
             ioe.printStackTrace();
         }
         catch (Exception e) {
-            System.out.println("Неустановленная ошибка");
+            System.err.println("Неустановленная ошибка");
             e.printStackTrace();
         }
         return "";
     }
 
-    public static List<String> parseText (String text) {
+    public static TreeMap<String, Integer> parseText (String text) {
         return Arrays.stream(text.split("[\s,.!?\";:+#=*/0-9@%—\\[\\]()\n\r\t]"))
                 .filter(s -> s.matches("[a-zA-Z'’а-яА-Я][a-zA-Z'’\\-а-яА-Я]*"))
-                .collect(Collectors.toList());
-    }
-
-    public static TreeMap<String, Integer> getWordStatsFromList (List <String> words) {
-        TreeMap<String, Integer> wordStats = new TreeMap<>(Collections.emptyMap());
-        for (String s : words) {
-            s = s.toLowerCase(Locale.ROOT);
-            if (wordStats.containsKey(s)) {
-                wordStats.replace(s, wordStats.get(s) + 1);
-            }
-            else {
-                wordStats.put(s, 1);
-            }
-        }
-        return wordStats;
-    }
-
-    public static void printTreeMap (TreeMap<String, Integer> treeMap) {
-        treeMap.forEach((k, v) -> System.out.println(k + " - " + v));
+                .map(s -> (Map.Entry<String, Integer>) new AbstractMap.SimpleEntry<>(s.toLowerCase(Locale.ROOT), 1))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Integer::sum, TreeMap::new));
     }
 }
